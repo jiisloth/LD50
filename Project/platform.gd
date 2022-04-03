7d07b1f8968d2898486f1c3f7c48090ef8722a6e
SkeletonJelly47 <mikkelsson4@gmail.com>
2022-04-03 13:09:27 +0300

    Convert tabs to spaces (superior)

--- Project/platform.gd
+++ Project/platform.gd
@@ -8,38 +8,38 @@ export(float) var distanceTillSunk = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_move_down(numOfObjects, delta)
-	if _checkIfSunken():
-		_destroy()
-	pass
+    _move_down(numOfObjects, delta)
+    if _checkIfSunken():
+        _destroy()
+    pass
 
 func _move_down(objectCount, delta):
-	var moveBy = -2 * objectCount * delta
-	translate(Vector3(0, moveBy, 0))
-	distanceTraveled += abs(moveBy)
-	pass
+    var moveBy = -2 * objectCount * delta
+    translate(Vector3(0, moveBy, 0))
+    distanceTraveled += abs(moveBy)
+    pass
 
 func _checkIfSunken():
-	if distanceTraveled >= distanceTillSunk:
-		return true
-	else:
-		print_debug(distanceTraveled)
-		return false
+    if distanceTraveled >= distanceTillSunk:
+        return true
+    else:
+        print_debug(distanceTraveled)
+        return false                    
 
 func _destroy():
-	queue_free()
+    queue_free()
 
 func _on_Area_body_entered(body):
-	if body.is_in_group("Weight"):
-		numOfObjects += 1
+    if body.is_in_group("Weight"):
+        numOfObjects += 1
 
 
 func _on_Area_body_exited(body):
-	if body.is_in_group("Weight"):
-		numOfObjects -= 1
+    if body.is_in_group("Weight"):
+        numOfObjects -= 1
